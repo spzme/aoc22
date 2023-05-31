586e1e19f4408bdc4048b8b5f29dd0aac5703a7a
Frank Groeneveld <frank@txtomedia.com>
2023-05-31 12:51:00 +0200

    Day 3 part 2 done

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -2,6 +2,7 @@ use core::str::FromStr;
 
 #[derive(Debug, PartialEq, Clone)]
 struct Item(usize);
+
 #[derive(Debug)]
 struct Rucksack(Vec<Item>);
 
@@ -49,7 +50,7 @@ fn find_both(rucksack: Rucksack) -> Result<Item, &'static str> {
     Err("No item that was in both compartments was found.")
 }
 
-fn main() {
+fn part1() {
     let mut sum = 0;
     for rucksack in include_str!("../files//input.txt")
         .lines()
@@ -61,5 +62,60 @@ fn main() {
         //println!("Duplicate item in both compartments: {:?} ", duplicate)
         sum += duplicate.0;
     }
-    println!("The sum of the duplicate items is {:?} ", sum);
+    println!("[PART1] The sum of the duplicate items is {:?} ", sum);
+}
+
+fn part2() {
+    //First, construct the groups of 3 rucksacks
+    let mut rucksacks: Vec<Vec<Rucksack>> = Vec::new();
+    rucksacks.push(Vec::new()); //first group
+    let mut count = 0;
+    for rucksack in include_str!("../files//input.txt")
+        .lines()
+        .map(|line| line.parse::<Rucksack>())
+    {
+        let rucksack = rucksack.unwrap();
+
+        if count > 2 {
+            rucksacks.push(Vec::new());
+            count = 0;
+        }
+        let group_to_add_to = rucksacks.last_mut().unwrap();
+        group_to_add_to.push(rucksack);
+        count += 1;
+    }
+
+    println!("number of groups: {:?}", rucksacks.len());
+
+    let mut sum = 0;
+
+    //Now, determine the common item in the group.
+    for group in rucksacks {
+        let common_element = determine_common_element(group);
+        sum += common_element.0;
+    }
+
+    println!("The sum of the common elements equals {:?}", sum);
+}
+
+fn determine_common_element(rucksacks: Vec<Rucksack>) -> Item {
+    let rucksack = rucksacks.first().unwrap();
+    let items = rucksack.0.clone();
+
+    let items2 = rucksacks.get(1).unwrap().0.clone();
+    let items3 = rucksacks.get(2).unwrap().0.clone();
+
+    for item in items {
+        if items2.contains(&item) && items3.contains(&item) {
+            return item;
+        }
+    }
+
+    Item(0)
+}
+
+fn main() {
+    part1();
+
+    part2();
 }
